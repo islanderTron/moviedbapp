faeb4efc7d45214b85848db706de910a739cb203
Paul Uncangco <islandertron@gmail.com>
2023-11-14 20:53:28 -0500

    Added a simple image

--- app/page.tsx
+++ app/page.tsx
@@ -8,32 +8,70 @@ import { useEffect, useState } from 'react';
 export default function Home() {
 	const [popular, setPopular] = useState(null)
 	const [isLoading, setLoading] = useState(true)
+
+	const [imageURL, setImageURL] = useState('');
 	
+	// Lifecycle methods
 	useEffect(() => {
-		fetch('/api/tmdb/popular')
+		getData();
+		getImagePath()
+	},[])
+
+	// HTTP methods
+	function getData() {
+		return fetch('/api/tmdb/popular')
 			.then((res) => res.json())
 			.then((res) => {
-				console.log(res);
-				
 				setPopular(res.result.results)
 				setLoading(false)
 			})
-	},[])
+	}
 
-	console.log(typeof popular); // weird 
+	function getImagePath() {
+		return fetch('/api/tmdb/image_config')
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res);
+				setImageURL(res.url_path)
+			})
+	}
+	// Render methods
+	function popularRender() {
+		if(popular) {
+			const test = [popular];
+			console.log(test);
+			
+			return (
+				<div>
+					{/* {test.map(data => {
+						<p>hi</p>
+					})} */}
+				</div>
+			)
+		}
+	}
 	
 	// Need to figure out how to handle with the state outside render function. Look at your old repo and it might help you to refresh your mind. 
 	if(isLoading) return <p>loading...</p>
 	if(!popular) return <p>uh oh</p>
-
-  return ( 
-		<main>
-			Home page
-			<div>
-				{
-					popular[0].title
-				}
-			</div>
-		</main>
-	)
+	else {
+		return ( 
+			<main>
+				Home page
+				test 
+				{popularRender()}
+				<div>
+					{
+						// need to describe the type of this data but the result look fine as usual
+						popular.map((data: any) => 
+							<div>
+								<p key={data.title}>{data.title}</p>
+								<img src={`${imageURL}/${data.poster_path}`} />
+							</div>
+						)
+					}
+				</div>
+			</main>
+		)
+	}
 }
